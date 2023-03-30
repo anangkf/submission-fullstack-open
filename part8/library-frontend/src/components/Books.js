f17df174ebb3b19eccb7aef446334eb5735cdd75
anangkf_ <gonanggoneng@gmail.com>
2023-03-30 15:53:18 +0700

    exercise 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,10 +1,23 @@
 import { useQuery } from "@apollo/client"
+import { useState } from "react"
 import { ALL_BOOKS } from "../queries/queries"
 
 const Books = () => {
+  const [ filter, setFilter ] = useState(null)
   const { data } = useQuery(ALL_BOOKS)
 
   const books = data?.results
+
+  const booksToBePresented = filter
+    ? books.filter((book) => book.genres.includes(filter))
+    : books
+
+  let genres = books?.map((book) => book.genres)
+  genres = new Set(genres?.flat())
+
+  const handleFilter = (genre) => {
+    setFilter(genre)
+  }
 
   return (
     <div>
@@ -17,7 +30,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books?.map((a) => (
+          {booksToBePresented?.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -26,6 +39,16 @@ const Books = () => {
           ))}
         </tbody>
       </table>
+      <div>
+        {
+          [...genres].map((genre, idx) => {
+            return (
+              <button key={idx} onClick={() => handleFilter(genre)} >{genre}</button>
+            )
+          })
+        }
+        <button onClick={() => handleFilter(null)} >all genres</button>
+      </div>
     </div>
   )
 }
