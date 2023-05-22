d441f0476bc91d87e1a45a32f4454b0c0e56157b
anangkf_ <gonanggoneng@gmail.com>
2023-05-22 22:58:53 +0700

    exercise 9.19

--- part9/flight-diary/client/src/components/DiaryForm.tsx
+++ part9/flight-diary/client/src/components/DiaryForm.tsx
@@ -36,15 +36,49 @@ const DiaryForm: React.FC<DiaryFormProps> = ({ addDiary }) => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="date">
           date
-          <input type="text" name="date" id="date" value={date} onChange={handleChange} />
+          <input type="date" name="date" id="date" value={date} onChange={handleChange} />
         </label><br />
         <label htmlFor="visibility">
           visibility
-          <input type="text" name="visibility" id="visibility" value={visibility} onChange={handleChange} />
+          <span>
+            <input type="radio" name="visibility" id="great" value='great' onChange={handleChange} checked={formData.visibility === 'great'} />
+            <label htmlFor="great">great</label>
+          </span>
+          <span>
+            <input type="radio" name="visibility" id="good" value='good' onChange={handleChange} checked={formData.visibility === 'good'} />
+            <label htmlFor="good">good</label>
+          </span>
+          <span>
+            <input type="radio" name="visibility" id="ok" value='ok' onChange={handleChange} checked={formData.visibility === 'ok'} />
+            <label htmlFor="ok">ok</label>
+          </span>
+          <span>
+            <input type="radio" name="visibility" id="poor" value='poor' onChange={handleChange} checked={formData.visibility === 'poor'} />
+            <label htmlFor="poor">poor</label>
+          </span>
         </label><br />
         <label htmlFor="weather">
           weather
-          <input type="text" name="weather" id="weather" value={weather} onChange={handleChange} />
+          <span>
+            <input type="radio" name="weather" id="sunny" value='sunny' onChange={handleChange} checked={formData.weather === 'sunny'} />
+            <label htmlFor="sunny">sunny</label>
+          </span>
+          <span>
+            <input type="radio" name="weather" id="rainy" value='rainy' onChange={handleChange} checked={formData.weather === 'rainy'} />
+            <label htmlFor="rainy">rainy</label>
+          </span>
+          <span>
+            <input type="radio" name="weather" id="ok" value='cloudy' onChange={handleChange} checked={formData.weather === 'cloudy'} />
+            <label htmlFor="cloudy">cloudy</label>
+          </span>
+          <span>
+            <input type="radio" name="weather" id="stormy" value='stormy' onChange={handleChange} checked={formData.weather === 'stormy'} />
+            <label htmlFor="stormy">stormy</label>
+          </span>
+          <span>
+            <input type="radio" name="weather" id="windy" value='windy' onChange={handleChange} checked={formData.weather === 'windy'} />
+            <label htmlFor="windy">windy</label>
+          </span>
         </label><br />
         <label htmlFor="comment">
           comment
